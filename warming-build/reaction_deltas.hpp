6e64e2521e8f0175b530a29876b0a81b96461dbd
Maxwell Brown <maxwell@maxwellnbrown.com>
2019-05-01 11:25:57 -0700

    Create simple warming model, 0K to 310K

---
+++ warming-build/reaction_deltas.hpp
@@ -0,0 +1,24 @@
+#include "utility/common_utils.hpp"
+#include "core/reaction.hpp"
+
+#include <cstddef>
+
+/*
+
+Define each reaction's reactants and products in reaction_deltas.hpp.
+Say a reaction enumerated as R_ONE has the following chemical formula:
+
+                           2A + B --> C
+
+The proper way to define that reaction's state change vector is as follows:
+
+STATIC_VAR int num_deltas_R_ONE = 3;
+STATIC_VAR int deltas_R_ONE[] = {-2, -1, 1};
+STATIC_VAR specie_id delta_ids_R_ONE[] = {A, B, C};
+
+*/
+
+STATIC_VAR int num_deltas_warming = 2;
+STATIC_VAR int deltas_warming[] = {1, -1};
+STATIC_VAR specie_id delta_ids_warming[] = {subject_energy, delta_env_energy};
+
