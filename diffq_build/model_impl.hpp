76a3626b6546d2491830faad6eb4f20f21e8bc20
Ruilong Zhuang <ruilong96@gmail.com>
2019-05-11 20:33:57 -0700

    Fix: my model now handles negative reaction rate

--- diffq_build/model_impl.hpp
+++ diffq_build/model_impl.hpp
@@ -9,6 +9,7 @@
 #include "sim/base.hpp"
 //#include "context.hpp"
 #include <cstddef>
+#include <cmath>
 
 /*
 
@@ -45,116 +46,136 @@ RATETYPE reaction<A1_syn>::active_rate(const Ctxt& c) {
 template<>
 template<class Ctxt>
 RATETYPE reaction<A0A1A2_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A0A1A2_assoc)  * (c.getCritVal(rcrit_A2) - (c.getCon(A0)) - (c.getCon(A1))) ;
+    auto x = c.getRate(A0A1A2_assoc)* (c.getCritVal(rcrit_A2) - (c.getCon(A0)) + c.getCon(A1));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A1A2A3_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A1A2A3_assoc)  * (c.getCritVal(rcrit_A3) - (c.getCon(A1)) - (c.getCon(A2))) ;
+    auto x = c.getRate(A1A2A3_assoc)* (c.getCritVal(rcrit_A3) - (c.getCon(A1)) + c.getCon(A2));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A2A3A4_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A2A3A4_assoc)  * (c.getCritVal(rcrit_A4) - (c.getCon(A2)) - (c.getCon(A3))) ;
+    auto x = c.getRate(A2A3A4_assoc)* (c.getCritVal(rcrit_A4) - (c.getCon(A2)) + c.getCon(A3));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A3A4A5_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A3A4A5_assoc)  * (c.getCritVal(rcrit_A5) - (c.getCon(A3)) - (c.getCon(A4))) ;
+    auto x = c.getRate(A3A4A5_assoc)* (c.getCritVal(rcrit_A5) - (c.getCon(A3)) + c.getCon(A4));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A4A5A6_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A4A5A6_assoc)  * (c.getCritVal(rcrit_A6) - (c.getCon(A4)) - (c.getCon(A5))) ;
+    auto x = c.getRate(A4A5A6_assoc)* (c.getCritVal(rcrit_A6) - (c.getCon(A4)) + c.getCon(A5));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A5A6A7_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A5A6A7_assoc)  * (c.getCritVal(rcrit_A7) - (c.getCon(A5)) - (c.getCon(A6))) ;
+    auto x = c.getRate(A5A6A7_assoc)* (c.getCritVal(rcrit_A7) - (c.getCon(A5)) + c.getCon(A6));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A6A7A8_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A6A7A8_assoc)  * (c.getCritVal(rcrit_A8) - (c.getCon(A6)) - (c.getCon(A7))) ;
+    auto x = c.getRate(A6A7A8_assoc)* (c.getCritVal(rcrit_A8) - (c.getCon(A6)) + c.getCon(A7));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A7A8A9_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A7A8A9_assoc)  * (c.getCritVal(rcrit_A9) - (c.getCon(A7)) - (c.getCon(A8))) ;
+    auto x = c.getRate(A7A8A9_assoc)* (c.getCritVal(rcrit_A9) - (c.getCon(A7)) + c.getCon(A8));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A8A9A10_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A8A9A10_assoc)  * (c.getCritVal(rcrit_A10) - (c.getCon(A8)) - (c.getCon(A9))) ;
+    auto x = c.getRate(A8A9A10_assoc)* (c.getCritVal(rcrit_A10) - (c.getCon(A8)) + c.getCon(A9));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A9A10A11_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A9A10A11_assoc)  * (c.getCritVal(rcrit_A11) - (c.getCon(A9)) - (c.getCon(A10))) ;
+    auto x = c.getRate(A9A10A11_assoc)* (c.getCritVal(rcrit_A11) - (c.getCon(A9)) + c.getCon(A10));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A10A11A12_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A10A11A12_assoc)  * (c.getCritVal(rcrit_A12) - (c.getCon(A10)) - (c.getCon(A11))) ;
+    auto x = c.getRate(A10A11A12_assoc)* (c.getCritVal(rcrit_A12) - (c.getCon(A10)) + c.getCon(A11));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A11A12A13_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A11A12A13_assoc)  * (c.getCritVal(rcrit_A13) - (c.getCon(A11)) - (c.getCon(A12))) ;
+    auto x = c.getRate(A11A12A13_assoc)* (c.getCritVal(rcrit_A13) - (c.getCon(A11)) + c.getCon(A12));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A12A13A14_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A12A13A14_assoc)  * (c.getCritVal(rcrit_A14) - (c.getCon(A12)) - (c.getCon(A13))) ;
+    auto x = c.getRate(A12A13A14_assoc)* (c.getCritVal(rcrit_A14) - (c.getCon(A12)) + c.getCon(A13));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A13A14A15_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A13A14A15_assoc)  * (c.getCritVal(rcrit_A15) - (c.getCon(A13)) - (c.getCon(A14))) ;
+    auto x = c.getRate(A13A14A15_assoc)* (c.getCritVal(rcrit_A15) - (c.getCon(A13)) + c.getCon(A14));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A14A15A16_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A14A15A16_assoc)  * (c.getCritVal(rcrit_A16) - (c.getCon(A14)) - (c.getCon(A15))) ;
+    auto x = c.getRate(A14A15A16_assoc)* (c.getCritVal(rcrit_A16) - (c.getCon(A14)) + c.getCon(A15));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A15A16A17_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A15A16A17_assoc)  * (c.getCritVal(rcrit_A17) - (c.getCon(A15)) - (c.getCon(A16))) ;
+    auto x = c.getRate(A15A16A17_assoc)* (c.getCritVal(rcrit_A17) - (c.getCon(A15)) + c.getCon(A16));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A16A17A18_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A16A17A18_assoc)  * (c.getCritVal(rcrit_A18) - (c.getCon(A16)) - (c.getCon(A17))) ;
+    auto x = c.getRate(A16A17A18_assoc)* (c.getCritVal(rcrit_A18) - (c.getCon(A16)) + c.getCon(A17));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A17A18A19_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A17A18A19_assoc)  * (c.getCritVal(rcrit_A19) - (c.getCon(A17)) - (c.getCon(A18))) ;
+    auto x = c.getRate(A17A18A19_assoc)* (c.getCritVal(rcrit_A19) - (c.getCon(A17)) + c.getCon(A18));
+    return std::max(x, 0.0);
 }
 
 template<>
 template<class Ctxt>
 RATETYPE reaction<A18A19A20_assoc>::active_rate(const Ctxt& c) {
-    return c.getRate(A18A19A20_assoc)  * (c.getCritVal(rcrit_A20) - (c.getCon(A18)) - (c.getCon(A19))) ;
+    auto x = c.getRate(A18A19A20_assoc)* (c.getCritVal(rcrit_A20) - (c.getCon(A18)) + c.getCon(A19));
+    return std::max(x, 0.0);
 }
+
 
 
 #endif // MODEL_IMPL_H
