73542bef3cca65be285ef708af2a7979a3f72538
Maxwell Brown <maxwell@maxwellnbrown.com>
2019-05-13 21:13:25 -0700

    Use C++11 constructs in place of C++14

--- source/sim/stoch/indexed_priority_queue.hpp
+++ source/sim/stoch/indexed_priority_queue.hpp
@@ -18,13 +18,13 @@ namespace stochastic {
       enum class ignore {};
 
       template <typename E>
-      using underlying_if_enum = std::conditional_t<
+      using underlying_if_enum = typename std::conditional<
         std::is_enum<E>::value,
-        std::underlying_type_t<
-          std::conditional_t<std::is_enum<E>::value, E, ignore>
-        >,
+        typename std::underlying_type<
+          typename std::conditional<std::is_enum<E>::value, E, ignore>::type
+        >::type,
         E
-      >;
+      >::type;
 
     public:
 
@@ -45,7 +45,7 @@ namespace stochastic {
         _max_size{static_cast<node_type>(max_size)},
         _compare{compare},
         _heap(_max_size),
-        _map(_max_size, null_node()) {};
+        _map(_max_size, null_node()) {}
 
       indexed_priority_queue(indexed_priority_queue const&) = default;
 
@@ -63,11 +63,11 @@ namespace stochastic {
 
       size_type size() const {
         return _size;
-      };
+      }
 
       bool empty() const {
         return _size == 0;
-      };
+      }
 
       void push(value_type value) {
         auto& node = node_at(value.first);
@@ -77,7 +77,7 @@ namespace stochastic {
         }
         value_of(node) = value;
         sift_up(node) || sift_down(node);
-      };
+      }
 
       void pop() {
         if (empty()) return;
@@ -89,28 +89,28 @@ namespace stochastic {
         --_size;
         sift_down(node);
         node_at(i) = null_node();
-      };
+      }
 
       const_iterator begin() const {
         return iterator_for(root());
-      };
+      }
 
       const_iterator end() const {
         return iterator_for(last()) + 1;
-      };
+      }
 
       const_reference top() const {
         return value_of(root());
-      };
+      }
 
       mapped_type const& operator[](index_type i) const {
         return value_of(node_at(i)).second;
-      };
+      }
 
       const_iterator find(index_type i) const {
         auto node = node_at(i);
         return node == null_node() ? end() : iterator_for(node);
-      };
+      }
 
       mapped_type const& at(index_type i) const {
         auto node = node_at(i);
@@ -118,12 +118,12 @@ namespace stochastic {
           throw std::out_of_range("Index out of range");
         }
         return value_of(node).second;
-      };
+      }
 
       template <typename... Args>
       void emplace(Args&&... args) {
         push(value_type(std::forward<Args>(args)...));
-      };
+      }
 
     private:
 
@@ -133,15 +133,15 @@ namespace stochastic {
 
       bool less(node_type a, node_type b) const {
         return _compare(value_of(a).second, value_of(b).second);
-      };
+      }
 
       node_type const& node_at(index_type i) const {
         return _map[static_cast<node_type>(i)];
-      };
+      }
 
       node_type& node_at(index_type i) {
         return const_cast<node_type &>(const_this().node_at(i));
-      };
+      }
 
       const_iterator iterator_for(node_type node) const {
         return _heap.data() + node;
@@ -153,39 +153,39 @@ namespace stochastic {
 
       const_reference value_of(node_type node) const {
         return *iterator_for(node);
-      };
+      }
 
       reference value_of(node_type node) {
         return const_cast<reference>(const_this().value_of(node));
-      };
+      }
 
-      static constexpr node_type root() { return 0; };
-      node_type last() const { return _size - 1; };
+      static constexpr node_type root() { return 0; }
+      node_type last() const { return _size - 1; }
 
       node_type parent_of(node_type node) const {
         return ((node + 1) >> 1) - 1;
-      };
+      }
 
       node_type left_of(node_type node) const {
         return (node << 1) + 1;
-      };
+      }
 
       node_type right_of(node_type node) const {
         return (node + 1) << 1;
-      };
+      }
 
       node_type min_child_of(node_type node) const {
         auto left = left_of(node);
         auto right = right_of(node);
         return (right < _size && less(right, left)) ? right : left;
-      };
+      }
 
       void swap(node_type a, node_type b) {
         using std::swap;
         swap(value_of(a), value_of(b));
         node_at(value_of(a).first) = a;
         node_at(value_of(b).first) = b;
-      };
+      }
 
       bool sift_up(node_type node) {
         node_type start = node, parent;
@@ -194,7 +194,7 @@ namespace stochastic {
           node = parent;
         }
         return node != start;
-      };
+      }
 
       bool sift_down(node_type node) {
         node_type start = node, min_child;
@@ -203,13 +203,15 @@ namespace stochastic {
           node = min_child;
         }
         return node != start;
-      };
+      }
 
       node_type _max_size;
 
+      mapped_compare _compare{};
+
       node_type null_node() const {
         return _max_size;
-      };
+      }
 
       std::vector<value_type> _heap;
 
@@ -217,7 +219,6 @@ namespace stochastic {
 
       node_type _size = 0;
 
-      mapped_compare _compare{};
   };
 
 }
